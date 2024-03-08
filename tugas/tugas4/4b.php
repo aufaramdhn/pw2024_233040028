1cb41baf9d813dae099af794f1deaa9c68dd3b87
aufaramdhn <ramadhanaufa3@gmail.com>
2024-03-08 18:53:34 +0700

    Menyelesaikan tugas 4

---
+++ tugas/tugas4/4b.php
@@ -0,0 +1,34 @@
+<?php
+
+$hardwares = ["Motherboard", "Processor", "Hard Disk", "PC Coller", "VGA Card", "SSD"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 4b | Pemrograman Web</title>
+</head>
+
+<body>
+    <h3>Macam-macam perangkat keras komputer</h3>
+    <ol>
+        <?php foreach ($hardwares as $hardware) : ?>
+            <li><?= $hardware ?></li>
+        <?php endforeach ?>
+    </ol>
+    <h3>Macam-macam perangkat keras komputer baru</h3>
+    <ol>
+        <?php
+        array_push($hardwares, "Card Reader", "Modem");
+        asort($hardwares);
+        foreach ($hardwares as $hardware) : ?>
+            <li><?= $hardware ?></li>
+        <?php endforeach ?>
+    </ol>
+</body>
+
+</html>
